72752ac9708cb9230c47cc6cd92dcde6a3414637
Siddharth Goyal <50585327+SidGoyal2014@users.noreply.github.com>
2020-05-24 22:56:48 -0700

    Create main.cpp

---
+++ HackerRank/Cavity map/main.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+#define CONDITION_1 a[i][j] > a[i-1][j] && a[i][j] > a[i][j-1]
+#define CONDITION_2 a[i][j] > a[i][j+1] && a[i][j] > a[i+1][j]
+int main(){
+    int n,i,j;
+    char a[1000][1000];
+    cin>>n;
+    for(i=1;i<=n;i++){
+        for(j=1;j<=n;j++){
+            cin>>a[i][j];
+        }
+    }
+    for(i=1;i<=n;i++){
+        for(j=1;j<=n;j++){
+            if((i!=1)&&(i!=n)&&(j!=1)&&(j!=n)){
+                if(CONDITION_1 && CONDITION_2){
+                    a[i][j]='X';
+                }
+            }
+        }
+    }
+    for(i=1;i<=n;i++){
+        for(j=1;j<=n;j++){
+            cout<<a[i][j];
+        }
+        cout<<endl;
+    }
+    return 0;
+}
