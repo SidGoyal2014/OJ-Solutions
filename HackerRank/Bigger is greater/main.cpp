a2e12038b869dc429b939ffbf893b627d29c7339
Siddharth Goyal <50585327+SidGoyal2014@users.noreply.github.com>
2020-05-24 22:57:34 -0700

    Create main.cpp

---
+++ HackerRank/Bigger is greater/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main(){
+    string s[100000];
+    int n,i;
+    cin>>n;
+    for(i=0;i<n;i++){
+        cin>>s[i];
+        bool val=next_permutation(s[i].begin(),s[i].end());
+        if(val==true){
+            cout<<s[i];
+        } 
+        else{
+            cout<<"no answer";
+        } 
+        if(i<(n-1)){
+            cout<<endl;
+        }
+    }
+    return 0;
+}
