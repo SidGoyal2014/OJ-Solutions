f1940ab7633642b056267ced51280f59b850ed61
Siddharth Goyal <50585327+SidGoyal2014@users.noreply.github.com>
2020-05-24 22:58:32 -0700

    Create main.cpp

---
+++ HackerRank/Counting Valleys/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n,i,arr[1000000];
+    char c[1000000];
+    cin>>n;
+    for(i=0;i<n;i++){
+        cin>>c[i];
+        if(c[i]=='U'){
+            arr[i]=1;
+        }
+        else{
+            arr[i]=-1;
+        }
+        
+    }
+
+    int first,sum=0,count=0;
+    first=arr[0];
+    for(i=0;i<n;i++){
+        sum=sum+arr[i];
+        if((sum==0)&&(first==-1)){
+            count++;
+            first = arr[i+1];
+        }
+        else if((sum==0)&&(first==1)){
+            first = arr[i+1];
+        }
+    }
+    cout<<count;
+    return 0;
+}
