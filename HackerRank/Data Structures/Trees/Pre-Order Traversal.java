eef2c36048976c6a537ff0bc3d46f5bcdc150071
Siddharth Goyal <50585327+SidGoyal2014@users.noreply.github.com>
2021-05-04 21:10:16 +0530

    Create Pre-Order Traversal.java

---
+++ HackerRank/Data Structures/Trees/Pre-Order Traversal.java
@@ -0,0 +1,25 @@
+// Question Link: https://www.hackerrank.com/challenges/tree-preorder-traversal/problem
+
+    /* you only have to complete the function given below.  
+        Node is defined as  
+
+        class Node {
+            int data;
+            Node left;
+            Node right;
+        }
+    */
+
+    public static void preOrder(Node root) {
+        
+        System.out.print(root.data + " ");
+        
+        if(root.left != null){
+            preOrder(root.left);
+        }
+        
+        if(root.right != null){
+            preOrder(root.right);
+        }
+
+    }
