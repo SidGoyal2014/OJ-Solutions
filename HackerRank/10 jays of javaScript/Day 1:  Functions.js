180474f40ecf702bd1f6f4cdcbdf18620aff34df
Siddharth Goyal <50585327+SidGoyal2014@users.noreply.github.com>
2021-05-07 14:05:51 +0530

    Create Day 1:  Functions.js

---
+++ HackerRank/10 jays of javaScript/Day 1:  Functions.js
@@ -0,0 +1,16 @@
+/*
+ * Create the function factorial here
+ */
+
+var dp = [];
+
+function factorial(n){
+    dp[0] = 1;
+    dp[1] = 1;
+    
+    for(var i = 2;i<100;i++){
+        dp[i] = i * dp[i-1];
+    }
+    
+    return dp[n];
+}
