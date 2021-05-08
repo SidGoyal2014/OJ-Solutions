0465af5d8699b4801b00b5b4d02e201c29728468
Siddharth Goyal <50585327+SidGoyal2014@users.noreply.github.com>
2021-05-08 23:04:01 +0530

    Create Day 4: Classes.js

---
+++ HackerRank/10 jays of javaScript/Day 4: Classes.js
@@ -0,0 +1,21 @@
+// Problem Link: https://www.hackerrank.com/challenges/js10-class/problem
+
+/*
+ * Implement a Polygon class with the following properties:
+ * 1. A constructor that takes an array of integer side lengths.
+ * 2. A 'perimeter' method that returns the sum of the Polygon's side lengths.
+*/
+
+class Polygon{
+    constructor(arr){
+        this.arr = arr;
+    }
+    
+    perimeter(){
+        var p = 0;
+        for(var i=0;i<(this.arr).length;i++){
+            p = p + (this.arr)[i];
+        }
+        return p;
+    }
+}
