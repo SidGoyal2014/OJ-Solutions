d300cf6f75eb129e5602215b48d7ace1eedacc07
Siddharth Goyal <50585327+SidGoyal2014@users.noreply.github.com>
2021-05-08 23:02:42 +0530

    Update Day 4: Count Objects.js

--- HackerRank/10 jays of javaScript/Day 4: Count Objects.js
+++ HackerRank/10 jays of javaScript/Day 4: Count Objects.js
@@ -1,21 +1,21 @@
 // Problem Link: https://www.hackerrank.com/challenges/js10-count-objects/problem
 
 /*
- * Implement a Polygon class with the following properties:
- * 1. A constructor that takes an array of integer side lengths.
- * 2. A 'perimeter' method that returns the sum of the Polygon's side lengths.
- */
+ * Return a count of the total number of objects 'o' satisfying o.x == o.y.
+ * 
+ * Parameter(s):
+ * objects: an array of objects with integer properties 'x' and 'y'
+*/
 
-class Polygon{
-    constructor(arr){
-        this.arr = arr;
+function getCount(objects) {
+    var n = objects.length;
+    var cnt = 0;
+    
+    for(var i=0;i<n;i++){
+        if(objects[i].x == objects[i].y){
+            cnt = cnt + 1;
+        }
     }
     
-    perimeter(){
-        var p = 0;
-        for(var i=0;i<(this.arr).length;i++){
-            p = p + (this.arr)[i];
-        }
-        return p;
-    }
+    return cnt;
 }
