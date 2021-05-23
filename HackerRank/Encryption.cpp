ab797610250ccece96b05770585f611b03b6d3af
Siddharth Goyal <50585327+SidGoyal2014@users.noreply.github.com>
2021-05-23 23:57:10 +0530

    Update and rename HackerRank/Encryption/main.cpp to HackerRank/Encryption.cpp

--- HackerRank/Encryption.cpp
+++ HackerRank/Encryption.cpp
@@ -1,3 +1,8 @@
+// Problem Name: Encryption
+// Problem Link: https://www.hackerrank.com/challenges/encryption/problem
+
+////////////// SOLUTION ////////////////
+
 #include <iostream>
 #include <string.h>
 #include <math.h>
